93eaa2b302bf2f5279e964a94978e2d6b5b9530f
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-04-07 20:15:18 -0300

    Testando escape sequences

---
+++ ex003/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    
+    <?php 
+    #0x = hexadecimal
+    #0b = binário
+    #0  = octal
+    #    $num = 0x1A;
+    #    echo "O número é $num"   
+    $v = 3e2;
+     echo "O valor é $v"
+    ?>
+</body>
+</html>
---
+++ tests/test002.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+        $nome = "Gustavo";
+        $snom = "Guanabara";
+        $nick = "Gafanhoto";
+        echo "$nome \"$nick\" $snom";
+
+    ?>
+</body>
+</html>
