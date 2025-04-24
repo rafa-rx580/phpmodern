68b716af9b616664cd5750ecc9af541618564e35
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-04-24 09:40:07 -0300

    BRL to USD converter
    BRL to USD converter using BCB API, now it's working properly!

--- challenges/d04/cadastro.php
+++ challenges/d04/cadastro.php
@@ -12,10 +12,27 @@
     </header>
     <main>
         <?php 
-            $nome = $_REQUEST["nome"] ?? "sem nome";
-            $snome = $_REQUEST["snome"] ?? "desconhecido";
-            echo "É um prazer te conhecer $nome $snome!";
+            $real = $_REQUEST["currency"] ?? 0;
+           
+
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+    
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+    
+            $dados = json_decode(file_get_contents($url), true);
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+            $dol = $real / $cotacao;
+
+            //Formatação de moedas com internacionalização;
+            $default = numfmt_create("pt-BR", NumberFormatter :: CURRENCY);
+            
+
+            echo "Seus " . numfmt_format_currency($default, $real, "BRL") . " equivalem a " . numfmt_format_currency($default, $dol, "USD") . ".";
+
         ?>
+        <p>Cotação obtida diretamente do site do <a href="https://www.bcb.gov.br/">Banco Central do Brasil</a>.</p>
         <p><a href="javascript:history.go(-1)">Voltar para a página anterior.</a></p>
     </main>
 </body>
