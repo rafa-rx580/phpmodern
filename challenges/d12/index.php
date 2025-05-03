1c1cf5582d07ae863af1a7ca09ca3f1a9cbd5c06
Rafael Gomes <156968191+rafa-rx580@users.noreply.github.com>
2025-05-03 09:17:57 -0300

    Tried to make the 12º exercise
    It's not actually working, i feel like a missed a important step inside the classes, but soon will fix it, i wish.

--- challenges/d12/index.php
+++ challenges/d12/index.php
@@ -7,6 +7,31 @@
     <title>Calculadora de tempo</title>
 </head>
 <body>
-    
+    <main>
+        <h1>Calculadora de tempo</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+            <label for="ts">Total de segundos: </label>
+            <input type="number" name="ts" id="ts">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <?php
+    $ts = $_REQUEST["ts"] ?? 0; 
+    $sm = intval($ts / 604800);
+    $d = intval($ts / 86400);
+    $h = intval($ts / 3600);
+    $m = intval($ts / 60);
+    $s = 0; 
+    ?>
+    <section>
+        <?php
+        echo "O total de <strong>$ts</strong> segundos equivale à:"; 
+        echo "<p>$sm Semanas</p>";
+        echo "<p>$d Dias</p>";
+        echo "<p>$h Horas</p>";
+        echo "<p>$m Minutos</p>";
+        echo "<p>$s Segundos</p>";
+        ?>
+    </section>
 </body>
 </html>
